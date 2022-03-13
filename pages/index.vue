8978eb1eef2cc86a39bcb4b8a77a269b31d2dd2f
jonnyparris <ruskin.constant+dev@gmail.com>
2022-03-13 16:36:21 +0000

    try and fail to align non-name column fields right

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
 .container
   div
     h1 Duvnab
-    BTable(:items='iAccounts', striped)
+    BTable(:items='iAccounts', :fields='columns', fixed, striped)
 </template>
 
 <script lang="ts">
@@ -12,16 +12,23 @@ export default defineComponent({
   setup() {
     const { app } = useContext()
     const iAccounts = ref<any>()
+    const columns = ref<any>()
 
     useFetch(async () => {
       try {
         iAccounts.value = await app.$axios.$get('investment-accounts')
+        columns.value = Object.keys(iAccounts.value[0]).map((k) => ({
+          label: k,
+          field: k,
+          class: k === 'name' ? '' : 'text-right',
+        }))
       } catch (e) {
         alert('data fetch booboo' + e)
       }
     })
     return {
       iAccounts,
+      columns,
     }
   },
 })
